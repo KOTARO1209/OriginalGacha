8879dedd132f1870aa7c562a16fc4ff22ce9ef25
KOTARO1209 <kmachida@mxdlab.net>
2023-03-15 02:50:36 +0900

    出やすさの数字に基づいて要素の出やすさを設定できるように！

--- OriginalGacha/GachaViewController.swift
+++ OriginalGacha/GachaViewController.swift
@@ -15,20 +15,20 @@ class GachaViewController: UIViewController {
     
     // 1. 遷移先に渡したい値を格納する変数を用意する
     var outputValue: [GachaItem]?
-    var rate_list: [Int]?
     
     // 配列の中に入っている要素の確率の合計値
     var calculationNumber: Int = 0
     var calculationRandomNumber: Int = 0
+    var now_rate: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         probabilityCalculation()
         
         // UIへの反映
-        self.objectNameLabel.text = outputValue![0].title
-        self.objectProbabilityLabel.text = String(outputValue![0].price)
-        self.objectExplanationTextView.text = outputValue![0].explanation
+        self.objectNameLabel.text = outputValue![now_rate].title
+        self.objectProbabilityLabel.text = String(outputValue![now_rate].price)
+        self.objectExplanationTextView.text = outputValue![now_rate].explanation
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,28 +39,21 @@ class GachaViewController: UIViewController {
     func probabilityCalculation() {
         for i in 0..<outputValue!.count {
             calculationNumber += outputValue![i].price
-            
-            //rate_listに確率の数字を追加してきたいけどなぜかできないいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい
-            //rate_list.insert(outputValue![i].price, at: i)
-            rate_list?.append(outputValue![i].price)
-            rate_list? += [1]
-            //rate_list?[i] = outputValue![i].price
-            print("iの値は",i)
         }
         
-        // UIへの反映の時に(0~数)としたいので意図的に-1している
-        calculationRandomNumber = Int.random(in: 1...calculationNumber) - 1
+        calculationRandomNumber = Int.random(in: 1...calculationNumber)
         
-        print("====")
-        print(calculationNumber)
-        print(rate_list)
-        print(calculationRandomNumber)
-    }
-    
-    func rateMake() {
+        for i in 0..<outputValue!.count {
+            now_rate += outputValue![i].price
+            if calculationRandomNumber <= now_rate {
+                now_rate = i
+                break
+            }
+        }
         
+        //print("now_rate=",now_rate)
+        print("ランダム=",calculationRandomNumber,"/",calculationNumber)
     }
-    
 }
 
 // [てぃーてぃー:1, えみりー:3, じねんじょ:3]
