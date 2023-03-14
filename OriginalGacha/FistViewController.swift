42c4cca11aa14408c6fb60f439c6be6933375266
KOTARO1209 <kmachida@mxdlab.net>
2023-03-14 21:21:04 +0900

    確率計算が完成、次は確率に従って出やすさを変えたい

--- OriginalGacha/FistViewController.swift
+++ OriginalGacha/FistViewController.swift
@@ -32,17 +32,13 @@ class FistViewController: UIViewController {
     // 選択したガチャの中身の名前を最初の画面に表示する
     func viewSetUp() {
         let title = GachaName.shared.title
-        print(title)
-        print(type(of: title))
         name = title
         self.gachaTitleLabel.text = title
     }
     
     func loadGachaData() {
-        print(self.name)
         category = GachaItem.shared.category
         gachaData = Array(realm.objects(GachaItem.self).filter("category == %@", category))
-        print(gachaData)
     }
     
     // segueが動作することをViewControllerに通知するメソッド
--- OriginalGacha/GachaViewController.swift
+++ OriginalGacha/GachaViewController.swift
@@ -14,21 +14,30 @@ class GachaViewController: UIViewController {
     @IBOutlet var objectExplanationTextView: UITextView!
     
     // 1. 遷移先に渡したい値を格納する変数を用意する
-    var outputValue : [GachaItem]?
+    var outputValue: [GachaItem]?
+    
+    // 配列の中に入っている要素の確率の合計値
+    var calculationNumber: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.objectNameLabel.text = outputValue![0].title
         self.objectProbabilityLabel.text = String(outputValue![0].price)
         self.objectExplanationTextView.text = outputValue![0].explanation
+        probabilityCalculation()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
+    // 確率の計算をしている
     func probabilityCalculation() {
-        
+        for i in 0..<outputValue!.count {
+            calculationNumber += outputValue![i].price
+        }
+        print(calculationNumber)
+        print(Int.random(in: 1...calculationNumber))
     }
     
 }
